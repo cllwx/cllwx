c9616dd9ff4625d23454590d9852d69b00d75f5e
Chris Lyndon <chrislukelyndon@gmail.com>
2019-09-02 20:08:24 +0930

    Starting trolleyTotal

---
+++ cllwx/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using cllwx.Utilities;
+
+namespace cllwx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpGet("trolleyTotal")]
+        public IActionResult GetLowestPossibleTotal(ActionExecutedContext context)
+        {
+            var httpUtil = new HttpReaders();
+            string bodyData = httpUtil.ReadBodyAsString(context.HttpContext.Request);
+            return Ok(bodyData);
+        }
+    }
+}
---
+++ cllwx/Utilities/HttpReaders.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cllwx.Utilities
+{
+    public interface IHttpReaders
+    {
+        string ReadBodyAsString(HttpRequest request);
+    }
+    public class HttpReaders
+    {
+        public string ReadBodyAsString(HttpRequest request)
+        {
+            var initialBody = request.Body; // Workaround
+
+            try
+            {
+                request.EnableRewind();
+
+                using (StreamReader reader = new StreamReader(request.Body))
+                {
+                    string text = reader.ReadToEnd();
+                    return text;
+                }
+            }
+            finally
+            {
+                // Workaround so MVC action will be able to read body as well
+                request.Body = initialBody;
+            }
+
+            return string.Empty;
+        }
+    }
+}
